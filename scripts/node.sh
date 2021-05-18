51330c98a1e8badea767d57ba372465e7690c397
Evan Shortiss <evanshortiss@gmail.com>
2021-05-18 10:44:16 +0100

    chore: update local dev script

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,6 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build -f Dockerfile.dev . -t shipwars-bot-server
 docker run -e AI_SERVER_URL=$AI_SERVER_URL \
--e GAME_SERVER_URL=ws://shipwars-server:8181/game -e HUSKY_SKIP_HOOKS=1 \
+-e GAME_SERVER_URL=ws://shipwars-game-server:8181/game -e HUSKY_SKIP_HOOKS=1 \
 --rm -p 8282:8282 -v "$(pwd)/src/:/usr/src/app/src/" \
 --name=shipwars-bot-server shipwars-bot-server
